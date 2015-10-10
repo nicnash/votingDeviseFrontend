ad5b3f88f676f55d39837c7412a925d67c5cab8f
Nic Nash <nicnash08@gmail.com>
2015-10-10 12:30:59 -0500

    add ideas route

--- app/ideas/route.js
+++ app/ideas/route.js
@@ -5,7 +5,9 @@ const { service } = Ember.inject;
 
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
   session: service('session'),
-
+  model: function() {
+    return bands;
+  },
   actions: {
     logout() {
       this.get('session').invalidate();
