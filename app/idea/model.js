041a8c65f956bdfe76cc24f56dd5b9262658b5fe
Nic Nash <nicnash08@gmail.com>
2015-10-10 22:00:31 -0500

    create user vote and idea model with relationships

--- app/idea/model.js
+++ app/idea/model.js
@@ -3,7 +3,9 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   title:        DS.attr('string'),
   description:  DS.attr('string'),
-  count:        DS.attr('number')
+  count:        DS.attr('number'),
+  user:         DS.belognsTo('user'),
+  vote:         DS.hasMany('vote')
 });
 
 
---
+++ app/vote/model.js
@@ -0,0 +1,6 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  user:      DS.belongsTo('user'),
+  idea:      DS.belongsTo('idea')
+});
