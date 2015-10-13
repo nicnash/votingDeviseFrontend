e6ba560d759aa53db43fe0ed4349beedd11f3eb6
Nic Nash <nicnash08@Nic-Nashs-iMac.local>
2015-10-12 20:56:06 -0500

    update data on vote save

--- app/ideas/controller.js
+++ app/ideas/controller.js
@@ -7,25 +7,28 @@ export default Ember.Controller.extend({
 		vote:function(ideaId){
 			console.log('---------vote',ideaId);
 			var self = this;
-			var idea = this.store.peekRecord('idea', ideaId);
-			// var count = self.get(idea.count);
-			console.log('ideaCount',idea.count);
-			idea.set('count', 55);
-			console.log('idea',idea);
-
-			// var sessionUser = self.get('sessionUser');
-			// console.log('sessionUser',sessionUser);
+			var idea = this.store.findRecord('idea', ideaId).then(function(idea) {
+				  var count = idea.get('count'); // => "Rails is Omakase"
+				   idea.set('count', count+1);
+				  // idea.save(); // => PUT to '/idea/1'
 
 
-			var store = this.store;
-			var aUser = self.store.peekRecord('user',1);
 
-			var newVote = store.createRecord('vote', {
-			  user: aUser,
-			  idea: idea,
-			});
-			console.log('newVote',newVote);
-			newVote.save();
+
+
+				  var aUser = self.store.peekRecord('user',1);
+
+				  var newVote = store.createRecord('vote', {
+				    user: aUser,
+				    idea: idea,
+				  });
+				  console.log('newVote',newVote);
+				  newVote.save();
+
+				  
+				});
+
+			
 		}
 	}
 });
