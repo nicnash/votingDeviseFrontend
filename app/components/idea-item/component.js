61f91a0b0e383cd29c269dfa7d1f5146b52ba32c
Nic Nash <nicnash08@gmail.com>
2015-10-12 23:39:44 -0500

    css updates, added component css, added idea item component

--- app/components/idea-item/component.js
+++ app/components/idea-item/component.js
@@ -1,4 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+    actions:{
+        vote:function(ideaId){
+            this.sendAction('vote',ideaId);
+        }
+    }
 });
