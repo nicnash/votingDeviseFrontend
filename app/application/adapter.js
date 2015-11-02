5e0c51c6d5cfe5d97ab52fb70edc9a267a4d3595
Nic Nash <nicnash08@gmail.com>
2015-11-01 18:56:23 -0600

    set environment correctly

--- app/application/adapter.js
+++ app/application/adapter.js
@@ -1,7 +1,9 @@
 import DS from 'ember-data';
 import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 import { ActiveModelAdapter } from 'active-model-adapter';
+import config from '../config/environment';
 
 export default ActiveModelAdapter.extend(DataAdapterMixin, {
-  authorizer: 'authorizer:devise'
+  authorizer: 'authorizer:devise',
+  host: config.api.host,
 });
--- app/authenticators/devise.js
+++ app/authenticators/devise.js
@@ -1,5 +1,7 @@
 import Devise from 'ember-simple-auth/authenticators/devise';
+import config from '../config/environment';
 
 export default Devise.extend({
   // serverTokenEndpoint: 'http://localhost:3000/users/sign_in'
+  serverTokenEndpoint: config.api.host + '/users/sign_in'
 });
